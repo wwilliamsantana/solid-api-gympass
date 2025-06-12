7095c32c9800490ab2f17bf38055901788b0ef9e
William Santana <euwilliamsantana@gmail.com>
2025-06-12 12:01:27 -0300

    feat: Add schemas models prisma

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('Server is running!')
 })
