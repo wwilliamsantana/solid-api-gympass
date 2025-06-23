7b50d0667f200c782298e2b9ac6af39b6e481603
William Santana <euwilliamsantana@gmail.com>
2025-06-23 11:33:49 -0300

    feat: Create Controller Authenticate

---
+++ src/http/controller/authenticate.ts
@@ -0,0 +1,37 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+import { z } from 'zod'
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-user-repository'
+import { AuthenticateUser } from '@/services/authenticate'
+import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const userRepository = new PrismaUsersRepository()
+    const registerUseCase = new AuthenticateUser(userRepository)
+
+    await registerUseCase.execute({
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  return reply.status(201).send()
+}
